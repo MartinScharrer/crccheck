1a1546b9bfeb71f313142eda8aec08ebafb894f3
Martin Scharrer <martin.scharrer@web.de>
2016-03-27 11:48:57 +0200

    Set v0.2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ config = {
     'url': 'https://bitbucket.org/martin_scharrer/checksum',
     'download_url': 'https://bitbucket.org/martin_scharrer/checksum/downloads/checksum.zip',
     'author_email': 'martin@scharrer-online.de',
-    'version': '0.1',
+    'version': '0.2',
     'install_requires': [],
     'packages': ['checksum'],
     'scripts': [],
