042a292c582a0fe2dfc77372c5b5d41e5e381bce
Martin Scharrer <martin.scharrer@web.de>
2020-06-18 18:33:53 +0200

    Added CRC-21/CAN-FD.

--- crccheck/crc.py
+++ crccheck/crc.py
@@ -847,6 +847,17 @@ class Crc17CanFd(CrcBase):
     _check_result = 0x04f03
 
 
+class Crc21CanFd(CrcBase):
+    """CRC-21/CAN-FD"""
+    _width = 21
+    _poly = 0x102899
+    _initvalue = 0x00000
+    _reflect_input = False
+    _reflect_output = False
+    _xor_output = 0x000000
+    _check_result = 0x0ed841
+
+
 class Crc24(CrcBase):
     """CRC-24"""
     _width = 24
@@ -1040,8 +1051,8 @@ ALLCRCCLASSES = (
     Crc10Cdma2000, Crc11, Crc123Gpp, Crc12Cdma2000, Crc12Dect, Crc13Bbc, Crc14Darc, Crc15, Crc15Mpt1327, Crc16, CrcArc,
     Crc16AugCcitt, Crc16Buypass, Crc16CcittFalse, Crc16Cdma2000, Crc16Dds110, Crc16DectR, Crc16DectX, Crc16Dnp,
     Crc16En13757, Crc16Genibus, Crc16Maxim, Crcc16Mcrf4xx, Crc16Riello, Crc16T10Dif, Crc16Teledisk, Crc16Tms37157,
-    Crc16Usb, CrcA, Crc16Ccitt, CrcKermit, CrcModbus, CrcX25, CrcXmodem, Crc17CanFd, Crc24, Crc24FlexrayA, Crc24FlexrayB,
-    Crc31Philips, Crc32, Crc32Bzip2, Crc32c, Crc32d, Crc32Mpeg2, Crc32Posix, Crc32q, CrcJamcrc, CrcXfer, Crc40Gsm,
-    Crc64, Crc64We, Crc64Xz, Crc82Darc
+    Crc16Usb, CrcA, Crc16Ccitt, CrcKermit, CrcModbus, CrcX25, CrcXmodem, Crc17CanFd, Crc21CanFd, Crc24, Crc24FlexrayA,
+    Crc24FlexrayB, Crc31Philips, Crc32, Crc32Bzip2, Crc32c, Crc32d, Crc32Mpeg2, Crc32Posix, Crc32q, CrcJamcrc, CrcXfer,
+    Crc40Gsm, Crc64, Crc64We, Crc64Xz, Crc82Darc
 )
 
