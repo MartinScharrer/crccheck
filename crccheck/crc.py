1b003aef9b3f3710bc8d07fee8faecddf9aa91c6
Martin Scharrer <martin.scharrer@web.de>
2020-06-24 07:49:04 +0200

    Added CRC-24/LTE-A and CRC-24/LTE-B.

--- crccheck/crc.py
+++ crccheck/crc.py
@@ -902,6 +902,28 @@ class Crc24Ble(CrcBase):
     _check_result = 0xc25a56
 
 
+class Crc24LteA(CrcBase):
+    """CRC-24/LTE-A"""
+    _width = 24
+    _poly = 0x864cfb
+    _initvalue = 0x000000
+    _reflect_input = False
+    _reflect_output = False
+    _xor_output = 0x000000
+    _check_result = 0xcde703
+
+
+class Crc24LteB(CrcBase):
+    """CRC-24/LTE-B"""
+    _width = 24
+    _poly = 0x800063
+    _initvalue = 0x000000
+    _reflect_input = False
+    _reflect_output = False
+    _xor_output = 0x000000
+    _check_result = 0x23ef52
+
+
 class Crc31Philips(CrcBase):
     """CRC-31/PHILIPS"""
     _width = 31
@@ -1063,7 +1085,7 @@ ALLCRCCLASSES = (
     Crc16AugCcitt, Crc16Buypass, Crc16CcittFalse, Crc16Cdma2000, Crc16Dds110, Crc16DectR, Crc16DectX, Crc16Dnp,
     Crc16En13757, Crc16Genibus, Crc16Maxim, Crcc16Mcrf4xx, Crc16Riello, Crc16T10Dif, Crc16Teledisk, Crc16Tms37157,
     Crc16Usb, CrcA, Crc16Ccitt, CrcKermit, CrcModbus, CrcX25, CrcXmodem, Crc17CanFd, Crc21CanFd, Crc24, Crc24FlexrayA,
-    Crc24FlexrayB, Crc24Ble, Crc31Philips, Crc32, Crc32Bzip2, Crc32c, Crc32d, Crc32Mpeg2, Crc32Posix, Crc32q, CrcJamcrc,
-    CrcXfer, Crc40Gsm, Crc64, Crc64We, Crc64Xz, Crc82Darc
+    Crc24FlexrayB, Crc24Ble, Crc24LteA, Crc24LteB, Crc31Philips, Crc32, Crc32Bzip2, Crc32c, Crc32d, Crc32Mpeg2,
+    Crc32Posix, Crc32q, CrcJamcrc, CrcXfer, Crc40Gsm, Crc64, Crc64We, Crc64Xz, Crc82Darc
 )
 
