ad31723940c49640b3a6dc2e0c9905d771a5e33f
Martin Scharrer <martin.scharrer@web.de>
2020-07-08 19:01:45 +0200

    Added CRC-24/OS-9.

--- crccheck/crc.py
+++ crccheck/crc.py
@@ -948,6 +948,18 @@ class Crc24OpenPgp(CrcBase):
     _residue = 0x000000
 
 
+class Crc24Os9(CrcBase):
+    """CRC-24/OS-9"""
+    _width = 24
+    _poly = 0x800063
+    _initvalue = 0xffffff
+    _reflect_input = False
+    _reflect_output = False
+    _xor_output = 0xffffff
+    _check_result = 0x200fa5
+    _residue = 0x800fe3
+
+
 class Crc31Philips(CrcBase):
     """CRC-31/PHILIPS"""
     _width = 31
@@ -1109,7 +1121,8 @@ ALLCRCCLASSES = (
     Crc16AugCcitt, Crc16Buypass, Crc16CcittFalse, Crc16Cdma2000, Crc16Dds110, Crc16DectR, Crc16DectX, Crc16Dnp,
     Crc16En13757, Crc16Genibus, Crc16Maxim, Crcc16Mcrf4xx, Crc16Riello, Crc16T10Dif, Crc16Teledisk, Crc16Tms37157,
     Crc16Usb, CrcA, Crc16Ccitt, CrcKermit, CrcModbus, CrcX25, CrcXmodem, Crc17CanFd, Crc21CanFd, Crc24, Crc24FlexrayA,
-    Crc24FlexrayB, Crc24Ble, Crc24Interlaken, Crc24LteA, Crc24LteB, Crc24OpenPgp, Crc31Philips, Crc32, Crc32Bzip2,
-    Crc32c, Crc32d, Crc32Mpeg2, Crc32Posix, Crc32q, CrcJamcrc, CrcXfer, Crc40Gsm, Crc64, Crc64We, Crc64Xz, Crc82Darc
+    Crc24FlexrayB, Crc24Ble, Crc24Interlaken, Crc24LteA, Crc24LteB, Crc24OpenPgp, Crc24Os9, Crc31Philips, Crc32,
+    Crc32Bzip2, Crc32c, Crc32d, Crc32Mpeg2, Crc32Posix, Crc32q, CrcJamcrc, CrcXfer, Crc40Gsm, Crc64, Crc64We, Crc64Xz,
+    Crc82Darc
 )
 
