cb218319bcb9329472500acb7cd4b42ca7a2ac2e
Martin Scharrer <martin.scharrer@web.de>
2020-07-20 21:22:45 +0200

    Updated copyright year.

--- crccheck/base.py
+++ crccheck/base.py
@@ -2,7 +2,7 @@
 
   License::
 
-    Copyright (C) 2015-2016 by Martin Scharrer <martin@scharrer-online.de>
+    Copyright (C) 2015-2020 by Martin Scharrer <martin@scharrer-online.de>
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
